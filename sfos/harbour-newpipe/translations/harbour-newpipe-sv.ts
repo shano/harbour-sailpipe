483d62b1a50ddda57ef74e742634cd89de37785f
Leif-Jöran Olsson <info@friprogramvarusyndikatet.se>
2025-08-16 17:03:56 +0200

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (66 of 66 strings)
    
    Translation: harbour-newpipe/harbour-newpipe
    Translate-URL: https://hosted.weblate.org/projects/harbour-newpipe/harbour-newpipe/sv/

--- sfos/harbour-newpipe/translations/harbour-newpipe-sv.ts
+++ sfos/harbour-newpipe/translations/harbour-newpipe-sv.ts
@@ -116,123 +116,123 @@
     </message>
     <message id="newpipe-channeltab-header_about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Om</translation>
     </message>
     <message id="newpipe-channeltab-header_videos">
         <source>Videos</source>
-        <translation type="unfinished"></translation>
+        <translation>Videor</translation>
     </message>
     <message id="newpipe-channeltab-placeholder_no-videos">
         <source>No videos</source>
-        <translation type="unfinished"></translation>
+        <translation>Inga videor</translation>
     </message>
     <message id="newpipe-channeltab-header_shorts">
         <source>Shorts</source>
-        <translation type="unfinished"></translation>
+        <translation>Snuttar</translation>
     </message>
     <message id="newpipe-channeltab-placeholder_no-shorts">
         <source>No shorts</source>
-        <translation type="unfinished"></translation>
+        <translation>Inga snuttar</translation>
     </message>
     <message id="newpipe-channeltab-header_live">
         <source>Live</source>
-        <translation type="unfinished"></translation>
+        <translation>Direktsändning</translation>
     </message>
     <message id="newpipe-channeltab-placeholder_no-livestreams">
         <source>No livestreams</source>
-        <translation type="unfinished"></translation>
+        <translation>Inga direktsändningar</translation>
     </message>
     <message id="newpipe-channeltab-header_playlists">
         <source>Playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Spellistor</translation>
     </message>
     <message id="newpipe-channeltab-placeholder_no-playlists">
         <source>No playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Inga spellistor</translation>
     </message>
     <message id="newpipe-channeltab-header_albums">
         <source>Albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Album</translation>
     </message>
     <message id="newpipe-channeltab-placeholder_no-albums">
         <source>No albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Inga album</translation>
     </message>
     <message id="newpipe-utils_length_unknown">
         <source>??:??:??</source>
-        <translation type="unfinished"></translation>
+        <translation>??:??:??</translation>
     </message>
     <message id="newpipe_filter_page-page_header">
         <source>Search Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Sökfilter</translation>
     </message>
     <message id="newpipe_search_page-menu_about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Om</translation>
     </message>
     <message id="newpipe_search_page-menu_filter">
         <source>Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Filter</translation>
     </message>
     <message id="newpipe_search_page-search_placeholder">
         <source>Search</source>
-        <translation type="unfinished">Sök</translation>
+        <translation>Sök</translation>
     </message>
     <message id="newpipe_search_page-search_no_entries">
         <source>No entries</source>
-        <translation type="unfinished">Inga poster</translation>
+        <translation>Inga poster</translation>
     </message>
     <message id="newpipe_search_page-search_enter_some_text">
         <source>Enter some text to search</source>
-        <translation type="unfinished">Ange sökuttryck</translation>
+        <translation>Ange sökuttryck</translation>
     </message>
     <message id="newpipe-filter-name_all">
         <source>All</source>
-        <translation type="unfinished"></translation>
+        <translation>Allt</translation>
     </message>
     <message id="newpipe-filter-name_videos">
         <source>Videos</source>
-        <translation type="unfinished"></translation>
+        <translation>Videor</translation>
     </message>
     <message id="newpipe-filter-name_channels">
         <source>Channels</source>
-        <translation type="unfinished"></translation>
+        <translation>Kanaler</translation>
     </message>
     <message id="newpipe-filter-name_playlists">
         <source>Playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Spellistor</translation>
     </message>
     <message id="newpipe-filter-name_music-congs">
         <source>Music songs</source>
-        <translation type="unfinished"></translation>
+        <translation>Låtar</translation>
     </message>
     <message id="newpipe-filter-name_music-videos">
         <source>Music videos</source>
-        <translation type="unfinished"></translation>
+        <translation>Musikvideor</translation>
     </message>
     <message id="newpipe-filter-name_music-albums">
         <source>Music albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Låtalbum</translation>
     </message>
     <message id="newpipe-filter-name_music-playlists">
         <source>Music playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Låtspellistor</translation>
     </message>
     <message id="newpipe_search_page-menu_service">
         <source>Service</source>
-        <translation type="unfinished"></translation>
+        <translation>Tjänst</translation>
     </message>
     <message id="newpipe_service_page-page_header">
         <source>Service</source>
-        <translation type="unfinished"></translation>
+        <translation>Tjänst</translation>
     </message>
     <message id="newpipe-channeltab-header_tracks">
         <source>Tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Spår</translation>
     </message>
     <message id="newpipe-channeltab-placeholder_no-tracks">
         <source>No tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Inga spår</translation>
     </message>
     <message id="newpipe_channel_details-detail_subscribers">
         <source>Subscribers</source>
@@ -256,31 +256,31 @@
     </message>
     <message id="newpipe_media_details-detail_views_not_applicable">
         <source>N/A</source>
-        <translation type="unfinished"></translation>
+        <translation>Ej tillämpbar</translation>
     </message>
     <message id="newpipe_media_details-detail_likes_not_applicable">
         <source>N/A</source>
-        <translation type="unfinished"></translation>
+        <translation>Ej tillämpbar</translation>
     </message>
     <message id="newpipe_media_details-detail_uploader">
         <source>Uploader</source>
-        <translation type="unfinished">Uppladdare</translation>
+        <translation>Uppladdare</translation>
     </message>
     <message id="newpipe_media_details-detail_date">
         <source>Upload date</source>
-        <translation type="unfinished">Uppladdningsdatum</translation>
+        <translation>Uppladdningsdatum</translation>
     </message>
     <message id="newpipe_media_details-detail_length">
         <source>Length</source>
-        <translation type="unfinished">Längd</translation>
+        <translation>Längd</translation>
     </message>
     <message id="newpipe_media_details-detail_category">
         <source>Category</source>
-        <translation type="unfinished">Kategori</translation>
+        <translation>Kategori</translation>
     </message>
     <message id="newpipe_media_details-detail_licence">
         <source>Licence</source>
-        <translation type="unfinished">Licens</translation>
+        <translation>Licens</translation>
     </message>
     <message id="newpipe_media_details-detail_description">
         <source>Description</source>
@@ -288,18 +288,18 @@
     </message>
     <message id="newpipe_channel_details-detail_subscribers_not_applicable">
         <source>N/A</source>
-        <translation type="unfinished"></translation>
+        <translation>Ej tillämpbar</translation>
     </message>
     <message id="newpipe_comment_item-replies" numerus="yes">
         <source>%n replies</source>
-        <translation type="unfinished">
+        <translation>
             <numerusform>%n svar</numerusform>
             <numerusform>%n svar</numerusform>
         </translation>
     </message>
     <message id="newpipe_cover-title">
         <source>NewPipe</source>
-        <translation type="unfinished">NewPipe</translation>
+        <translation>NewPipe</translation>
     </message>
     <message id="newpipe_playlist_page-header">
         <source>YouTube Playlist</source>
@@ -307,7 +307,7 @@
     </message>
     <message id="newpipe_playlist-no_entries">
         <source>No entries</source>
-        <translation type="unfinished">Inga poster</translation>
+        <translation>Inga poster</translation>
     </message>
     <message id="newpipe_playlist-no_videos">
         <source>There are no videos in this playlist</source>
@@ -315,41 +315,41 @@
     </message>
     <message id="newpipe_replies-page_header">
         <source>Replies</source>
-        <translation type="unfinished">Svar</translation>
+        <translation>Svar</translation>
     </message>
     <message id="newpipe_media-page_header_video">
         <source>%0 Video</source>
         <oldsource>%1 Video</oldsource>
-        <translation type="unfinished">YouTube-video</translation>
+        <translation>Video: %0</translation>
     </message>
     <message id="newpipe_media-page_header_audio">
         <source>%0 Audio</source>
         <oldsource>%1 Audio</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Låt: %0</translation>
     </message>
     <message id="newpipe_media-page_header_media">
         <source>Media</source>
-        <translation type="unfinished"></translation>
+        <translation>Media</translation>
     </message>
     <message id="newpipe_channel-page_header_channel">
         <source>%0 Channel</source>
-        <translation type="unfinished"></translation>
+        <translation>Kanal: %0</translation>
     </message>
     <message id="newpipe_channel-page_header_user">
         <source>%0 User</source>
-        <translation type="unfinished"></translation>
+        <translation>Användare: %0</translation>
     </message>
     <message id="newpipe_channel-page_header_artist">
         <source>%0 Artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Artist: %0</translation>
     </message>
     <message id="newpipe_channel-page_header_channel_default">
         <source>Channel</source>
-        <translation type="unfinished"></translation>
+        <translation>Kanal</translation>
     </message>
     <message id="newpipe_playlist_page-header_playlist">
         <source>%0 Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Spellista: %0</translation>
     </message>
 </context>
 </TS>
