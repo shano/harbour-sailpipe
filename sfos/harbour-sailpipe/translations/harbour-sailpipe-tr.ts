be07fd2d964e9ecd2df08c385b9e738e1e84038d
Leif-Jöran Olsson <info@friprogramvarusyndikatet.se>
2025-11-15 11:54:51 +0100

    Translated using Weblate (Turkish)
    Currently translated at 51.8% (55 of 106 strings)
    
    Translation: harbour-newpipe/harbour-newpipe
    Translate-URL: https://hosted.weblate.org/projects/harbour-newpipe/harbour-newpipe/tr/

--- sfos/harbour-sailpipe/translations/harbour-sailpipe-tr.ts
+++ sfos/harbour-sailpipe/translations/harbour-sailpipe-tr.ts
@@ -194,11 +194,11 @@
     </message>
     <message id="sailpipe_search_page-menu_service">
         <source>Service</source>
-        <translation>Hizmet</translation>
+        <translation type="unfinished">Hizmet</translation>
     </message>
     <message id="sailpipe_service_page-page_header">
         <source>Service</source>
-        <translation>Hizmet</translation>
+        <translation type="unfinished">Hizmet</translation>
     </message>
     <message id="sailpipe-channeltab-header_tracks">
         <source>Tracks</source>
