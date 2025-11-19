f434ab56197af55e8bd5c9d9b55bde90878ebfae
Leif-Jöran Olsson <info@friprogramvarusyndikatet.se>
2025-11-19 22:31:13 +0100

    Translated using Weblate (Slovak)
    Currently translated at 99.0% (106 of 107 strings)
    
    Translation: harbour-newpipe/harbour-newpipe
    Translate-URL: https://hosted.weblate.org/projects/harbour-newpipe/harbour-newpipe/sk/

--- sfos/harbour-sailpipe/translations/harbour-sailpipe-sk.ts
+++ sfos/harbour-sailpipe/translations/harbour-sailpipe-sk.ts
@@ -384,7 +384,7 @@
     <message id="sailpipe_about-summary">
         <source>A user interface to the NewPipe Extractor for streaming and downloading video and music from multiple online services, including YouTube, SoundCloud, Media.ccc.de and Bandcamp.</source>
         <oldsource>A user interface to the NewPipe Extractor for streaming and downloading video and music from multiple online services, including YouTube, SoundCloud, Media.cc.de and Bandcamp.</oldsource>
-        <translation type="unfinished">Používateľské rozhranie NewPipe Extractora pre streamovanie a sťahovanie videí a pesničiek z niekoľkých online služieb, vrátane YouTube, SoundCloudu, Media.ccc.de a Bandcampu.</translation>
+        <translation>Používateľské rozhranie NewPipe Extractora pre streamovanie a sťahovanie videí a pesničiek z niekoľkých online služieb, vrátane YouTube, SoundCloudu, Media.ccc.de a Bandcampu.</translation>
     </message>
     <message id="sailpipe_about-website">
         <source>Website</source>
