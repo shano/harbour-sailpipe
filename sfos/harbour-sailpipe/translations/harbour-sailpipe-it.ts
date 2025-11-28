2c99282f6bf45f106e0b047c0095a24e6f700b8a
Leif-Jöran Olsson <info@friprogramvarusyndikatet.se>
2025-11-28 12:29:22 +0100

    Translated using Weblate (Italian)
    Currently translated at 100.0% (107 of 107 strings)
    
    Translation: harbour-newpipe/harbour-newpipe
    Translate-URL: https://hosted.weblate.org/projects/harbour-newpipe/harbour-newpipe/it/

--- sfos/harbour-sailpipe/translations/harbour-sailpipe-it.ts
+++ sfos/harbour-sailpipe/translations/harbour-sailpipe-it.ts
@@ -529,7 +529,7 @@
     </message>
     <message id="sailpipe_about-poet">
         <source>Project poet</source>
-        <translation type="unfinished"></translation>
+        <translation>Poeta del progetto</translation>
     </message>
 </context>
 </TS>
